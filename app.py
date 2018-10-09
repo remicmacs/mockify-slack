42deb107263c25a7db43651f442c44731a6f89ce
remicmacs <do.remimi.fa@gmail.com>
2018-10-09 11:53:21 +0200

    Refactoring

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
+import requests
 from flask import Flask, jsonify, request
 from mockify import mockify
 
+
 app = Flask(__name__)
+
+def access_control(req):
+    blacklist = [
+        "U7D2XEMEC", # Gwendal
+        "U9A5V02AW", # Nelson (do NOT remove)
+        #"U7EG16H3R", # Vincent
+        "U7DNH2BUJ", # Rodolphe
+        # "U7DDC77SP", # Rémi
+    ]
+
+    # Filter blacklist
+    if (req.form["user_id"] in blacklist):
+        app.logger.info("Is in blacklist")
+        return jsonify(mockify(
+            "I'm sorry " + request.form["user_name"].split('.')[0]
+            + ", I'm afraid I can't do that"
+            )
+        )
+    else:
+        return None
+
+def log(req):
+    app.logger.info(
+        "/api/mockifyapp/ endpoint reached by \""
+        + "{:<25}".format(req.form["user_name"]) + "\", user_id: \""
+        + "{:9}".format(req.form["user_id"]) + "\""
+        + "; message: \"" + "{:<50}".format(req.form["text"]) + "\""
+        + " ; in channel \"" + "{:<20}".format(req.form["channel_name"])
+        + "\", channel_id: \"" + "{:9}".format(req.form["channel_id"]) + "\""
+    )
 
 @app.route("/")
 def hello():
@@ -29,21 +61,12 @@ def slackmock():
     Retrieve a POST HTTP request from Slack and mockify the text passed as
     parameter in the request.form["text"]
     '''
-    blacklist = [
-        "U7D2XEMEC", # Gwendal
-        "U9A5V02AW", # Nelson (do NOT remove)
-    ]
+    # Logging incoming request
+    log(request)
 
-    # Filter blacklist
-    if (request.form["user_id"] in blacklist):
-        return jsonify(mockify(
-            "I'm sorry " + request.form["user_name"].split('.')[0]
-            + ", I'm afraid I can't do that"
-            ))
-
-    app.logger.info('/api/mockifyapp/ endpoint reached')
-    app.logger.debug('Text payload: ' + request.form["text"])
-    app.logger.debug('User_id' + request.form["user_id"])
+    # Blacklisting
+    ctl = access_control(request)
+    if ctl is not None: return ctl
 
     # Forging request for delayed response
     req_payload = {
@@ -55,3 +78,13 @@ def slackmock():
 
     # Return 200 status to HTTP request (ack)
     return ""
+
+@app.route("/api/mockifyapp/bot/", methods=["POST"])
+def slack_mock_bot():
+    app.logger.info("/api/mockifyapp/bot/ endpoint reached")
+    app.logger.info(request.data["challenge"])
+    challenge = request.form["challenge"]
+    app.logger.info("Challenge retrieved: ")
+    app.logger.info(challenge)
+    return jsonify({"challenge": request.form["challenge"]})
+
