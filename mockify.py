f2fdd74d82d2080448eee408573a213da932a1c1
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-30 20:58:17 +0800

    Delete mockify.py

--- mockify.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def tossCoin(): return random.random() <= 0.5
-
-def randCase(c): return c.upper() if tossCoin() else c.lower()
-
-def mockify(s): return ''.join([randCase(el) for el in s])
